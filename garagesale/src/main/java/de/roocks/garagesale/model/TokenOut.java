15078781f3df5ceac7cfe07d5a8454bc1f6de6a4
braincooler <w.zeller@online.de>
2018-03-05 20:19:55 +0100

    bug fix

---
+++ garagesale/src/main/java/de/roocks/garagesale/model/TokenOut.java
@@ -0,0 +1,20 @@
+package de.roocks.garagesale.model;
+
+public class TokenOut {
+
+	private String value;
+
+	public TokenOut() {
+		super();
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	
+}
